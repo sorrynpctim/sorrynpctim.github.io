815abc18de6a6aa174042808ffa0c86c706a48ea
NPC Tim <163519401+0xnpctim@users.noreply.github.com>
2024-11-04 14:38:22 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const text = 'NPC Tim'
-const typingSpeed = 500;
+const text = 'Hi, my name is NPC Tim.';
+const typingSpeed = 75;
 let a = 0;
 
 function type() {
@@ -7,7 +7,25 @@ function type() {
         document.getElementById('centered').innerHTML += text.charAt(a);
         a++;
         setTimeout(type, typingSpeed);
+    } else {
+        fadeInLinks();
     }
 }
 
+function fadeInLinks() {
+    const links = document.getElementById('links');
+    links.classList.add('fade-in');     // Apply fade-in effect to #links
+    
+    // Fade in #status after #links has finished fading in
+    setTimeout(() => {
+        fadeInStatus();
+    }, 1500);     // Match the transition duration
+}
+
+function fadeInStatus() {
+    const status = document.getElementById('status');
+    status.classList.add('fade-in');    // Apply fade-in effect to #status
+}
+
+// Start the typing effect on window load
 window.onload = type;
